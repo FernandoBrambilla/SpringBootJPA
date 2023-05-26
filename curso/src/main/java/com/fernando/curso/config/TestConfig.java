c405330ed4d88782f902bbb67f5585bc8f1cf13e
FernandoBrambilla <ferbabdobrambillab1@hotmail.com>
2023-05-26 17:00:19 -0300

    ManyToMany association whith JoinTable

--- curso/src/main/java/com/fernando/curso/config/TestConfig.java
+++ curso/src/main/java/com/fernando/curso/config/TestConfig.java
@@ -45,23 +45,29 @@ public class TestConfig implements CommandLineRunner{
 		Category cat1 = new Category(null, "Eletronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		Product p1 = new Product(null, "The Lord of Rings", "The Book complete, ed 2023", 100.0, "");
 		Product p2 = new Product(null, "SmartTv", "Tv 50'' Led 4k", 1300.0, "");
 		Product p3 = new Product(null, "Notebook Acer", "I5 ssd480gb 8gb", 100.0, "");
+		productRepository.saveAll(Arrays.asList(p1,p2,p3));
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		productRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
-		
+		userRepository.saveAll(Arrays.asList(u1,u2));
 	
 		Order o2 = new Order(null, Instant.now(),OrderStatus.PAID, u2);
 		Order o3 = new Order(null, Instant.now(),OrderStatus.DELIVERED, u1);
-		
-		
-		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o2,o3));
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		productRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		
+		
+		
 	}
 
 }
