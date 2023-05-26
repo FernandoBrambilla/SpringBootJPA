c0e4fdd290ee2633d6ca62b7109aea668c2497e6
FernandoBrambilla <ferbabdobrambillab1@hotmail.com>
2023-05-26 13:43:06 -0300

    Instant ,ISO 8601

--- curso/src/main/java/com/fernando/curso/config/TestConfig.java
+++ curso/src/main/java/com/fernando/curso/config/TestConfig.java
@@ -1,5 +1,8 @@
 package com.fernando.curso.config;
 
+
+
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +10,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.fernando.curso.entities.Order;
 import com.fernando.curso.entities.User;
+import com.fernando.curso.repositories.OrderRepository;
 import com.fernando.curso.repositories.UserRepository;
 
 @Configuration
@@ -16,13 +21,21 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired	
+	private OrderRepository orderRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
-
+		
+	
+		Order o2 = new Order(null, Instant.now(), u2);
+		Order o3 = new Order(null, Instant.now(), u1);
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o2,o3));
 	}
 
 }
---
+++ curso/src/main/java/com/fernando/curso/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.fernando.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fernando.curso.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+	
+}
